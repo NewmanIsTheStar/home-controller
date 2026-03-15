c00065747a6fc77238ddb773d6c435555a0d0297
Newman <badnews_brown@comcast.net>
2026-03-15 15:22:59 -0500

    renamed project in cmakelist.txt  and workspace

--- home-controller/worker_tasks.c
+++ home-controller/worker_tasks.c
@@ -26,9 +26,8 @@
 WORKER_TASK_T worker_tasks[] =
 {
     //  function        name                    stack   priority        
-#ifdef INCORPORATE_HOME_CONTROLLER    
-    {   hc_task,        "Home Controller Task", 8096,   6},      
-    {   discovery_task, "Discovery Task",       8096,   7},          
+#ifdef INCORPORATE_THERMOSTAT    
+    {   thermostat_task,"Thermostat Task",      8096,   5},       
 #endif
 
     // end of table
